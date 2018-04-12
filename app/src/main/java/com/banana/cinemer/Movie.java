33255a60cb43299217b84c49cc7ec2595b3b8126
banandroid <ndrdbnn@gmail.com>
2018-04-12 23:04:24 +0300

    Update Movie.java

--- app/src/main/java/com/banana/cinemer/Movie.java
+++ app/src/main/java/com/banana/cinemer/Movie.java
@@ -2,10 +2,13 @@ package com.banana.cinemer;
 
 import java.io.Serializable;
 
+/**
+ * Класс для фильма.
+ */
 public class Movie implements Serializable {
 
-    public String title;
-    public String posterPath;
+    public String title; // заголовок фильма
+    public String posterPath; // относительный адрес картинки с постером
 
     public Movie(String title, String posterPath) {
         this.title = title;
